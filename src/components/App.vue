fa56e031a95a80836faee845932890109737d91d
Utolol22 <Pierre@digital-skipper.com>
2025-03-27 18:28:27 +0100

    Ajout du composant App.vue principal

---
+++ src/components/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <app-nav></app-nav>
+    <div class="container app-content">
+      <router-view></router-view>
+    </div>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppNav from './AppNav.vue'
+import AppFooter from './AppFooter.vue'
+</script>
+
+<style lang="scss">
+html {
+  position: relative;
+  min-height: 100%;
+}
+body {
+  /* Margin bottom by footer height */
+  margin-bottom: 60px;
+}
+.app-content {
+  padding: 60px 15px 0;
+}
+</style>
